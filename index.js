09056ff0ce5c032b6f8ac69cc1f81eba2c0b3106
Hope Banks <hopiebanks@gmail.com>
2018-11-06 09:14:18 -0500

    working on associations

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ let test = require('./controllers/testcontroller');
 let user = require('./controllers/usercontroller');
 let id = require('./controllers/idcontroller');
 let country = require('./controllers/countrycontroller');
+let destination = require('./controllers/destinationcontroller')
 let sequelize = require('./db');
 let bodyParser = require('body-parser');
 
-sequelize.sync(); //Pass in {force: true} for resetting tables.
+//sequelize.sync(); //Pass in {force: true} for resetting tables.
 
 index.use(bodyParser.json());
 index.use(require('./middleware/headers'));
@@ -18,7 +19,9 @@ index.use('/user', user);
 index.use('/test', test);
 index.use('/id', id);
 index.use('/country', country);
+index.use('/destination', destination);
+
 
 index.use(require('./middleware/validate-session'));
-
+require('./associations')
 index.listen(process.env.PORT, function(){console.log('We Live out on dat super secret server!')});
